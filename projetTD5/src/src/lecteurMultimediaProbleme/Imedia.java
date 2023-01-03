4e19dc8e9e5fb8acbfcacddb1d43019803f76eb4
Elbahjaoui Jihane <Jihane.Elbahjaoui@emsi-edu.ma>
2023-01-03 15:29:07 +0100

    first commit

--- projetTD5/src/src/lecteurMultimediaProbleme/Imedia.java
+++
@@ -1,5 +0,0 @@
-package src.lecteurMultimediaProbleme;
-
-public interface Imedia {
-    public void afficher();
-}
--- projetTD5/src/src/lecteurMultimediaProbleme/Pdf.java
+++
@@ -1,7 +0,0 @@
-package src.lecteurMultimediaProbleme;
-public class Pdf implements Imedia {
-	public void afficher() {
-
-		System.out.println("lisant pdf....");
-	}
-}
